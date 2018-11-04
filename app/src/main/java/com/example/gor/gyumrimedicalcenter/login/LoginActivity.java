0d138bf792cb4e00221fa4245e0c77318aa04122
Gor <gevorgyan604@gmail.com>
2018-11-04 16:12:54 +0400

    Add Project

---
+++ app/src/main/java/com/example/gor/gyumrimedicalcenter/login/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.gor.gyumrimedicalcenter.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+import com.example.gor.gyumrimedicalcenter.MainActivity;
+import com.example.gor.gyumrimedicalcenter.R;
+
+import java.util.ArrayList;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_login);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("  LOGIN  ");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.finish();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public  void onRegisterButtonClick(View view){
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+    public  void  onLoginButtonClick(View view){
+//        Intent intent = new Intent(this , Doctor.class);
+//        startActivity(intent);
+    }
+
+    public  static final ArrayList<Person> arrayPerson = new ArrayList<>();
+}
